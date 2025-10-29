48312fa2a1813db00215a95a198c8c42c4447f7c
NimaSon <132659237+NimaSon@users.noreply.github.com>
2025-10-29 19:18:25 +0500

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "./globals.css";
+
+export const metadata = {
+  title: "Многостраничный сайт",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>
+        <Header />
+        <main style={{ padding: 20 }}>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
