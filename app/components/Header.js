c786012d40efc24edfdd21df0005bfc25fc808be
NimaSon <abdimazhitzh@gmail.com>
2025-10-29 20:13:49 +0500

    Добавлен меню в app/components/Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,7 +3,6 @@ export default function Header() {
     <header style={{ padding: 20, background: "#eee" }}>
       <nav>
         <a href="/">Главная</a> |{" "}
-        <a href="/about">О нас</a> |{" "}
         <a href="/contact">Контакты</a> | {" "}
         <a href="/menu">Меню</a>
       </nav>
