6aabdd72f1b85e3d16ac81b99ec9221f7fa11480
NimaSon <132659237+NimaSon@users.noreply.github.com>
2025-10-29 20:26:50 +0500

    Fix link to menu in Header component

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
     <header style={{ padding: 20, background: "#eee" }}>
       <nav>
         <a href="/">Главная</a> |{" "}
-        <a href="/menu>Меню</a> |{" "}
+        <a href="/menu">Меню</a> |{" "}
         <a href="/contact">Контакты</a>
       </nav>
     </header>
