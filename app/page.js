4ddb3ef05cf0485ad8cb3e1d44fc231bda74b60a
NimaSon <abdimazhitzh@gmail.com>
2025-10-29 20:15:14 +0500

    Изменен файл app/page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 export default function Home() {
   return <h1>Добро пожаловать на главную страницу!</h1>
-=======
-export default function Home() {
-  return <h1>Добро пожаловать на главную страницу!</h1>
->>>>>>> 848a91a (Обновлена папка с новыми файлами)
 }
