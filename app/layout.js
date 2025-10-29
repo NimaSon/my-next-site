48c522fed557572e8d89517ba288632b9031b8fd
NimaSon <132659237+NimaSon@users.noreply.github.com>
2025-10-29 19:19:30 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,1 @@
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import "./globals.css";
 
-export const metadata = {
-  title: "Многостраничный сайт",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ru">
-      <body>
-        <Header />
-        <main style={{ padding: 20 }}>{children}</main>
-        <Footer />
-      </body>
-    </html>
-  );
-}
